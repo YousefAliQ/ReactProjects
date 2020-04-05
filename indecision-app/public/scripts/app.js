04231440422ee87403f6037094e82dde61f4420c
yousefaliq <yusuf.alquran@gmail.com>
2020-04-04 20:14:43 -0500

    add variables and objects to JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,11 +2,86 @@
 
 // JSX - JavaScript XML
 
-var template = React.createElement(
-  'p',
-  null,
-  'this is JSX Really'
+var templateOne = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'p',
+        null,
+        'This is JSX '
+    ),
+    React.createElement(
+        'h1',
+        null,
+        'I am just a paragraph'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Two'
+        )
+    )
 );
+
+// const userName = 'Yousef Ali';
+// const userAge = 27;
+// const userLocation = 'IOWA';
+
+var user = {
+    name: 'Yousef Ali',
+    age: 27,
+    location: 'IOWA'
+};
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Name : ',
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' Age : ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location : ',
+        user.location
+    )
+);
+
+var app = {
+    title: 'Indicision App',
+    subtitle: 'app for decisions'
+};
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'h3',
+        null,
+        app.subtitle
+    )
+);
+
 //var template = /*#__PURE__*/React.createElement("p", null, "this is JSX");
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,44 @@
 // JSX - JavaScript XML
 
-const template = <p>this is JSX Really</p>
+const templateOne = (
+    <div>
+        <p>This is JSX </p>
+        <h1>I am just a paragraph</h1>
+        <ol>
+            <li>One</li>
+            <li>Two</li>
+        </ol>
+    </div>);
+
+// const userName = 'Yousef Ali';
+// const userAge = 27;
+// const userLocation = 'IOWA';
+
+const user = {
+    name : 'Yousef Ali',
+    age : 27,
+    location : 'IOWA'
+}
+const templateTwo = (
+    <div>
+        <h1>Name : {user.name}</h1>
+        <p> Age : {user.age}</p>
+        <p>Location : {user.location}</p>
+    </div>
+);
+
+const app = {
+    title : 'Indicision App',
+    subtitle : 'app for decisions'
+}
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        <h3>{app.subtitle}</h3>
+    </div>
+);
+
 //var template = /*#__PURE__*/React.createElement("p", null, "this is JSX");
 const appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
+
