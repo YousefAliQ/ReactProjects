702f77a81f33dd062b2b41cb3720247358b8b348
yousefaliq <yusuf.alquran@gmail.com>
2020-04-29 02:44:49 -0500

    pass childre to component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,4 +3,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = (props) => {
+    return (
+        <div>
+            <p>header</p>
+            {props.children}
+            <p>footer</p>
+        </div>
+    );
+};
+
+ReactDOM.render((
+    <Layout>
+        <div>
+            <h1>Title</h1>
+            <p>I am paragraph</p>
+        </div>
+
+    </Layout>
+), document.getElementById('app'));
