0061afff10fab0670b9a4b48bc52f601733e3acc
yousefaliq <yusuf.alquran@gmail.com>
2020-04-29 03:32:17 -0500

    setup react modal

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,22 +3,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-const Layout = (props) => {
-    return (
-        <div>
-            <p>header</p>
-            {props.children}
-            <p>footer</p>
-        </div>
-    );
-};
-
-ReactDOM.render((
-    <Layout>
-        <div>
-            <h1>Title</h1>
-            <p>I am paragraph</p>
-        </div>
-
-    </Layout>
-), document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
