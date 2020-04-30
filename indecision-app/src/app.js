0dc1dcdb314fc5a8241a0646c4edcb83f3075d96
yousefaliq <yusuf.alquran@gmail.com>
2020-04-29 23:55:07 -0500

    setup webpack with scss

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,5 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import './styles/style.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render( < IndecisionApp / > , document.getElementById('app'));
