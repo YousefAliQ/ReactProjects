2b4f9caf6b13e43b3a1376501cdc7a981bfa21b9
yousefaliq <yusuf.alquran@gmail.com>
2020-05-06 01:43:06 -0500

    add links between routes

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,7 @@
 // install -> import -> use
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
@@ -31,18 +31,33 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
     <div>
-        I am the 404!
+        I am the 404! -- <Link to="/">Go Home</Link>
     </div>
 );
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="isActive" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="isActive">Create Expense</NavLink>
+        <NavLink to="/edit" activeClassName="isActive">Edit</NavLink>
+        <NavLink to="/help" activeClassName="isActive">Help</NavLink>
+    </header>
+);
+
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage}/>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage} />
             </Switch>
+        </div>
+
     </BrowserRouter>
 );
 
