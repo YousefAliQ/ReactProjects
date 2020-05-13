6571eaf843102db3558ab6604f9d90699b50f986
yousefaliq <yusuf.alquran@gmail.com>
2020-05-12 23:46:41 -0500

    add ES6 Object Destructuring

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -4,7 +4,7 @@
 const path = require('path');
 module.exports = {
     //entry: './src/app.js',
-    entry: './src/playground/redux-101.js',
+    entry: './src/playground/destructuring.js',
     output: {
         path: path.join(__dirname, 'public'), // absolute path
         filename: 'boundle.js'
