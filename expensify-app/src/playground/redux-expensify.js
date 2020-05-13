211f95fbb574902795eeae323226b5e6a249b2af
yousefaliq <yusuf.alquran@gmail.com>
2020-05-13 02:40:46 -0500

    add ES6 spread operator in reducers

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,7 +1,28 @@
 import { createStore, combineReducers } from 'redux';
+import { v4 as uuidv4 } from 'uuid';
 
-// ADD_EXPENSE
-// REMOVE_EXPENSE
+
+const addExpense = (
+    {
+        description = "",
+        note = "",
+        amount = 0,
+        createdAt = 0
+    } = {}) => ({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: uuidv4(),
+            description,
+            note,
+            amount,
+            createdAt
+        }
+    });
+
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
 // EDIT_EDPENSE
 
 const expensesReducerDefaultState = [];
@@ -9,6 +30,13 @@ const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
 
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ] // we can use .concat does not change the state like 'push' But it will produce a new array
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id);
         default:
             return state;
     }
@@ -41,7 +69,14 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const expense1 = store.dispatch(addExpense({ description: "Rent", amount: 100 }));
+const expense2 = store.dispatch(addExpense({ description: "Coffee", amount: 300 }));
+
+store.dispatch(removeExpense({ id: expense1.expense.id }));
 
 const demoState = {
     expenses: [{
