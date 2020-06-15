3dfed4db7913262ee05e3bca3776d8f530c92db0
yousefaliq <yusuf.alquran@gmail.com>
2020-06-14 23:34:46 -0500

    add types of Props

--- TypeScriptExamples/tsr/src/components/Head.tsx
+++ TypeScriptExamples/tsr/src/components/Head.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+type User = {
+    name: string;
+};
+
 type Props = {
     title: String; // required
     isActive?: boolean; // optional
+    thing?: number;
+    thing2?: string[];
+    status?: "loading" | "loaded";
+    thing3?: {};
+    thing4?: {
+        name: string;
+    };
+    func?: () => void;
+    user?: User;
 }
 
 export const Head = ({ title = "Hello", isActive = true }: Props) => {
