2ec536897f46a7d18a4ba355b80a11cab8d6f11c
yousefaliq <yusuf.alquran@gmail.com>
2020-06-15 08:34:29 -0500

    add typing with usestate

--- TypeScriptExamples/tsr/src/components/Input.tsx
+++ TypeScriptExamples/tsr/src/components/Input.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Input = () => {
-    return (
-        <input />
-    )
+    const [name, setName] = useState(""); // setState should not be a number!
+    //const [name, setName] = useState<string>(""); 
+    //const [name, setName] = useState<string | null>(null);    
+    return <input value={name} onChange={e => setName(e.target.value)} />;
 }
